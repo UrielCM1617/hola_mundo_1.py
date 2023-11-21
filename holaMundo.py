85b974bcf772d06c8559d376bde0250e3604493a
UrielCM1617 <125332082+UrielCM1617@users.noreply.github.com>
2023-11-21 09:25:45 -0600

    holaMundo.py

--- holaMundo.py
+++ holaMundo.py
@@ -1,1 +1,10 @@
+def saludar(nombre, matricula):
+    print(f"Hola Mundo, {nombre}!")
+    print(f"Tu número de matrícula es: {matricula}")
 
+if __name__ == "__main__":
+
+    nombre= 'Uriel Castillo'
+    matricula = 2223068846
+
+    print(saludar(nombre, matricula))
